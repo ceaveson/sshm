74b82366f005d7f7b90e60fb85a5f24852a8f4b0
CraigEaveson <craigeaveson@CE.lan>
2022-10-31 09:30:57 +0000

    added filters to show command

--- sshm.py
+++ sshm.py
@@ -11,11 +11,16 @@ import sys
 
 SSMHOSTS = ".sshmhosts.yaml"
 
+#TODO add help doc to each command and option
+#TODO add netbox integration
+#TODO add ssh session logging
+
 def create_hosts_dict(hosts_file):
     hosts = []
     try:
         with open(hosts_file, "r") as f:
             hosts = list(yaml.load_all(f, Loader=SafeLoader))
+            hosts.sort(key=lambda k: (k['type'], k['hostname']))
             for key, host in enumerate(hosts):
                 host["key"] = key
             return hosts
@@ -36,25 +41,28 @@ def cli():
 
 
 @click.command()
-@click.option('--hostname','-h', required=True)
-@click.option('--ip_address','-ip', required=True)
-def add(hostname, ip_address):
+@click.option("--hostname", "-h", required=True)
+@click.option("--ip_address", "-ip", required=True)
+@click.option("--type", "-t", required=True)
+def add(hostname, ip_address, type):
     try:
         IPv4Address(ip_address)
     except AddressValueError:
-        click.echo('Invalid IPv4 Address')
+        click.echo("Invalid IPv4 Address")
         sys.exit()
     table = Table(title="Added")
     table.add_column("Hostname", style="magenta")
     table.add_column("IP", justify="right", style="green")
+    table.add_column("Type", justify="right", style="blue")
     hosts = create_hosts_dict(SSMHOSTS)
-    host = {"hostname": hostname, "IP": ip_address, "key": None}
+    host = {"hostname": hostname, "IP": ip_address, "type" : type, "key": None}
     hosts.append(host)
     update_sshmhosts(hosts, SSMHOSTS)
-    table.add_row(host['hostname'],host['IP'])
+    table.add_row(host["hostname"], host["IP"], host["type"])
     console = Console()
     console.print(table)
     click.echo("added")
+
 
 @click.command()
 @click.argument("key")
@@ -66,22 +74,43 @@ def delete(key):
     host = [i for i in hosts if (i["key"] == int(key))][0]
     hosts = [i for i in hosts if not (i["key"] == int(key))]
     update_sshmhosts(hosts, SSMHOSTS)
-    table.add_row(host['hostname'],host['IP'])
+    table.add_row(host["hostname"], host["IP"])
     console = Console()
     console.print(table)
 
 
 @click.command()
-def show():
+@click.option("--hostname", "-h")
+@click.option("--type", "-t")
+def show(hostname:str, type:str):
     table = Table(title="hosts")
-    table.add_column('Key', justify="right", style="cyan", no_wrap=True)
+    table.add_column("Key", justify="right", style="cyan", no_wrap=True)
     table.add_column("Hostname", style="magenta")
     table.add_column("IP", justify="right", style="green")
+    table.add_column("Type", justify="right", style="blue")
     hosts = create_hosts_dict(SSMHOSTS)
+    if hostname:
+        hosts = [i for i in hosts if hostname in i['hostname']]
+    if type:
+        hosts = [i for i in hosts if type in i['type']]
     for host in hosts:
-        table.add_row(str(host["key"]), host["hostname"], host["IP"])
+        table.add_row(str(host["key"]), host["hostname"], host["IP"], host["type"])
     console = Console()
     console.print(table)
+
+
+@click.command()
+def types():
+    hosts = create_hosts_dict(SSMHOSTS)
+    types = set([i["type"] for i in hosts])
+    table = Table()
+    table.add_column("Types", justify="left", style="cyan", no_wrap=True)
+    for type in types:
+        table.add_row(type)
+    console = Console()
+    console.print(table)
+
+
 
 @click.command()
 @click.argument("key")
@@ -96,6 +125,7 @@ cli.add_command(add)
 cli.add_command(delete)
 cli.add_command(show)
 cli.add_command(connect)
+cli.add_command(types)
 
 if __name__ == "__main__":
     cli()
--- test_sshm.py
+++ test_sshm.py
@@ -23,16 +23,21 @@ def test_create_hosts_dict(tmp_path):
 
     assert len(hosts) == 3 and host["IP"] == "127.0.0.1"
 
+
 def test_update_sshmhosts(tmp_path):
     d = tmp_path / "sub"
     d.mkdir()
     p = d / sshmhosts_file
     p.write_text(sshmhosts_example)
-    
-    hosts = [{'IP': '1.0.0.127', 'hostname': 'not_loopback', 'key': 0}, {'IP': '3.3.3.3', 'hostname': 'host3', 'key': 1}, {'IP': '4.4.4.4', 'hostname': 'host4', 'key': 2}]
+
+    hosts = [
+        {"IP": "1.0.0.127", "hostname": "not_loopback", "key": 0},
+        {"IP": "3.3.3.3", "hostname": "host3", "key": 1},
+        {"IP": "4.4.4.4", "hostname": "host4", "key": 2},
+    ]
     update_sshmhosts(hosts, p)
 
     f = open(p)
     flist = f.readlines()
+
     assert len(flist) == 8 and [i for i in flist if "hostname: not_loopback" in i][0]
-        
