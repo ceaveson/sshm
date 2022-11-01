41c7b75c56567fc2a98072005d890485c5143a9c
CraigEaveson <craigeaveson@CE.lan>
2022-11-01 08:56:14 +0000

    converted to package

--- setup.py
+++ setup.py
@@ -1,15 +1,32 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='sshm',
-    version='0.1.0',
-    py_modules=['sshm'],
+    version='0.1.1',
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=[
-        'Click',
+        "attrs==22.1.0",
+        "click==8.1.3",
+        "commonmark==0.9.1",
+        "exceptiongroup==1.0.0",
+        "iniconfig==1.1.1",
+        "mypy-extensions==0.4.3",
+        "packaging==21.3",
+        "pathspec==0.10.1",
+        "platformdirs==2.5.2",
+        "pluggy==1.0.0",
+        "Pygments==2.13.0",
+        "pyparsing==3.0.9",
+        "pytest==7.2.0",
+        "PyYAML==6.0",
+        "rich==12.6.0",
+        "tomli==2.0.1",
+
     ],
     entry_points={
         'console_scripts': [
-            'sshm = sshm:cli',
+            'sshm = sshm.main:cli',
         ],
     },
 )
