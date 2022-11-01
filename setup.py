d226d6f3ead84f66bb8cc5dada8b0f3803fb57a6
ceaveson <ceaveson@gmail.com>
2022-11-01 14:52:36 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "attrs",
-        "Click",
+        "click",
         "commonmark",
         "exceptiongroup",
         "iniconfig",
